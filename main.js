7da43badddbf2f8f377ee7950c57722cf195743b
Miftahurrahman-STEI <mippganz@gamil.com>
2024-02-11 15:15:46 +0700

    Finished commit someone

---
+++ main.js
@@ -0,0 +1,7 @@
+const nama = prompt('Masukan Nama Kamu ');
+
+alert(`Haloo ${nama} Dipencet yaa`);
+alert('Penasaran yaaa?');
+alert('Okayy pencet sekali lagi ya');
+
+window.location.href = 'https://youtu.be/SctrVF37GAQ?si=jKEOV-hO7XDulS69';
